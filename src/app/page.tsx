7632db9e3a5e232089c492945e7b4e00dfb53991
donoh <201904014@o.cnu.ac.kr>
2023-06-20 21:51:46 +0900

    useRouter hook usage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,12 @@
+"use client";
+
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 export default function Home() {
+	const router = useRouter();
+	const [location, setLocation] = useState("");
 	return (
 		<main className="bg-gray-100 min-h-screen w-screen">
 			<main className="max-w-screen-2xl m-auto bg-white">
@@ -35,8 +41,18 @@ export default function Home() {
 									className="rounded  mr-3 p-2 w-[450px]"
 									type="text"
 									placeholder="State, city or town"
+									value={location}
+									onChange={(e) =>
+										setLocation(e.target.value)
+									}
 								/>
-								<button className="rounded bg-red-600 px-9 py-2 text-white">
+								<button
+									className="rounded bg-red-600 px-9 py-2 text-white"
+									onClick={() => {
+										if (location === "banana") return;
+										router.push("/search");
+									}}
+								>
 									Let's go
 								</button>
 							</div>
@@ -44,7 +60,7 @@ export default function Home() {
 						</div>
 					</div>
 					{/* HEADER */} {/* CARDS */}
-					<div className="py-3 px-36 mt-10 flex flex-wrap justify-center">
+					<div className="py-3 px-36 mt-10 flex flex-wrap justify-left gap-4">
 						{/* CARD */}
 						<Link href={"/restaurant/milestones-grill"}>
 							<div className="w-64 h-72 m-3 rounded overflow-hidden border cursor-pointer">
